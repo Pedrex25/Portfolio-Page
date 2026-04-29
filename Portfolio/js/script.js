3bc5d43a6e012139cca28c740fe4f60a822c1644
Pedrex25 <pedrobernardo7192@gmail.com>
2026-04-29 23:25:05 +0100

    Changed project file structure, it is now  possible to filter projects. Project data is now stored inside a xml file.

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,59 +1,164 @@
+const PROGRAMMING = "programming";
+const EMBBEDED = "embbeded";
+const ARCHITECTURE = "architecture";
+const COMPOSITIONS = "compositions";
+const EDITING_SOFTWARE = "editing_software";
+
+/**
+ * Gets the current year
+ */
 function getCurrentYear() {
     let current_year = new Date().getFullYear();
     document.getElementById("year").innerHTML = current_year;
 }
 
-// async function getProjects() {
-//     try {
-//         const response = await fetch("https://api.github.com/users/Pedrex25/repos");
-//         const container = document.getElementById("project-list");
-//         container.innerHTML = "";
+/**
+ * Reads the xml from the file containing all of my projects
+ * @param {any} selected_option receives a returned string from getSelectOption()
+ */
+async function readProjectXML(selected_option) {
+    try {
+        //Get xml file
+        const response = await fetch("assets/xml/projects.xml");
 
-//         if (!response.ok) {
-//             console.log(response.ok);
-//             throw new Error("Github Error");
-//         }
+        //Transform xml in text
+        const xml_text = await response.text();
 
-//         const projects = await response.json();
+        // Convert xml text into xml document
+        const parser = new DOMParser();
+        const xml_doc = parser.parseFromString(xml_text, "text/xml");
 
-//         for (const project of projects) {
-//             const getLanguages = await fetch(project.languages_url);
-//             const languagesList = await getLanguages.json();
+        const projects = xml_doc.getElementsByTagName("project");
 
-//             const languages = Object.keys(languagesList);
+        // Gets all projects that have the corresponding category
+        if (selected_option != "all") {
+            const matching_projects = xml_doc.querySelectorAll(`project[category="${selected_option}"]`);
 
-//             container.innerHTML += createProjectCard(project, languages);
-//         }
+            // Clear container before adding projects
+            const container = document.getElementById("project-list");
+            if (container)
+                container.innerHTML = "";
 
-//     } catch (error) {
-//         console.error("Error:", error);
-//     }
-// }
+            // iterates through each project within the choosen category
+            matching_projects.forEach(project => {
+                const project_title = project.querySelector("title").textContent;
+                const project_img = project.querySelector("img").textContent;
+                const project_link = project.querySelector("link").textContent;
+                const languages = project.querySelectorAll("language");
 
-// function createProjectCard(project, languages) {
-//     const imageURL = `https://opengraph.githubassets.com/1/Pedrex25/${project.name}`;
-//     const cardLanguages = languages.map(language => `
-//         <div class="card-language">
-//             <span class="card-dot" style="background-color: var(--${language.toLowerCase()}-color);"></span>
-//             <span>${language}</span>
-//         </div>
-//     `).join('');
+                createProjectCard(languages, project_title, project_img, project_link);
+            });
+        }
+        else {
+            const matching_projects = xml_doc.querySelectorAll("project");
 
-//     return `
-//         <div class="card">
-//             <img class="card-image" src="${imageURL}" alt="${project.name}" width="100%">
-//             <div class="card-container">
-//                 <div class="card-title">
-//                     <span>&nbsp;</span>
-//                     <h1>${project.name}</h1>
-//                 </div>
-//                 <div class="card-languages">
-//                     ${cardLanguages}
-//                 </div>
-//             </div>
-//         </div>
-//     `;
-// }
+            // Clear container before adding projects
+            const container = document.getElementById("project-list");
+            if (container)
+                container.innerHTML = "";
+
+            // iterates through each project within the choosen category
+            matching_projects.forEach(project => {
+                const project_title = project.querySelector("title").textContent;
+                const project_img = project.querySelector("img").textContent;
+                const project_link = project.querySelector("link").textContent;
+                const languages = project.querySelectorAll("language");
+
+                if (languages === null)
+                    languages = "";
+
+                createProjectCard(languages, project_title, project_img, project_link);
+            });
+        }
+    } catch (error) {
+        console.log("Error reading the XML: ", error);
+    }
+}
+
+/**
+ * Filters to the wanted project
+ */
+function getProjectXML() {
+    var selected_option = getSelectOption();
+    switch (selected_option) {
+        case PROGRAMMING:
+            readProjectXML(selected_option);
+            break;
+        case EMBBEDED:
+            readProjectXML(selected_option);
+            break;
+        case ARCHITECTURE:
+            readProjectXML(selected_option);
+            break;
+        case COMPOSITIONS:
+            readProjectXML(selected_option);
+            break;
+        case EDITING_SOFTWARE:
+            readProjectXML(selected_option);
+            break;
+        default:
+            readProjectXML(selected_option);
+            break;
+    }
+}
+
+/**
+ * Gets the value from the projects dropdown
+ * @returns the selected value from the dropdown containing all the project categories
+ */
+function getSelectOption() {
+    const categories = document.getElementById("projects");
+    const selected_value = categories.value;
+
+    return selected_value;
+}
+
+/**
+ * Creates cards in html, based on the number of projects or what was selected
+ * @param {any} stack languages used in a project
+ * @param {any} title title of the project
+ * @param {any} img image of the project
+ * @param {any} link link to the project
+ */
+function createProjectCard(stack, title, img, link) {
+    const container = document.getElementById("project-list");
+
+
+    let card_languages = "";
+    stack.forEach(language_node => {
+        var language_name = language_node.textContent;
+        var css_color = language_name
+
+        if (language_name.toLowerCase() === "c++")
+            css_color = "cpp"
+        else if (language_name.toLowerCase() === "c#")
+            css_color = "cs"
+
+        card_languages += `
+        <div class="card-language">
+            <span class="card-dot" style="background-color: var(--${css_color.toLowerCase()}-color);"></span>
+            <span>${language_name}</span>
+        </div>
+        `;
+    });
+
+    const card_project = `
+        <div class="card">
+            <img class="card-image" src="${img}" alt="${title}" width="100%">
+            <div class="card-container">
+                <div class="card-title">
+                    <span>&nbsp;</span>
+                    <a href="${link}" target="_blank">
+                        <h1>${title}</h1>
+                    </a>
+                </div>
+                <div class="card-languages">
+                    ${card_languages}
+                </div>
+            </div>
+        </div>
+    `;
+    container.innerHTML += card_project;
+}
 
 getCurrentYear();
-// getProjects();
